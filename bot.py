5f601229caddcc722383233b0669532f0052148f
Grant <grant@bourneadvisory.ca>
2021-01-07 14:31:34 -0500

    Update bot.py

---
+++ bot.py
@@ -0,0 +1,84 @@
+import os
+import random
+import discord
+
+from discord.ext import commands
+from dotenv import load_dotenv
+from discord import Member
+from discord.ext.commands import has_permissions, MissingPermissions
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+bot = commands.Bot(command_prefix=commands.when_mentioned_or("!"), description="The ultimate bored person bot", help_command = None, case_insensitive = True)
+
+print("Bot is starting...")
+
+@bot.event
+async def on_ready():
+
+    # Set bot status:
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="fued"))
+
+    print('\n\nLogged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('-------------------\n')
+
+#########################################COMMANDS#####################################################################
+
+@bot.command(name="help", description = "Learn what each command does.", pass_context=True) #help command
+@commands.cooldown(1, 5, commands.BucketType.guild)
+async def help(ctx, args=None):
+    help_embed = discord.Embed(title="Stuck? Join the Support Server!", color = 0x8a3f0a, url='https://discord.gg/vhGhEsDyCf')
+    command_names_list = [i.name for i in bot.commands]
+
+    # If there are no arguments, just list the commands:
+    if not args:
+        help_embed.add_field(
+            name="List of supported commands:",
+            value="\n".join([i.name for i in bot.commands]),
+            inline=False
+        )
+        help_embed.add_field(
+            name="Details",
+            value="The prefix for this bot is \"`!`\" -- Remember the space between the prefix and command!\n\nType `!help <command name>` for more details about a command.",
+            inline=False
+        )
+
+    # If is a valid command:
+    elif args in command_names_list:
+        help_embed.add_field(
+            name=args,
+            value=bot.get_command(args).description
+        )
+
+    # If else:
+    else:
+        help_embed.add_field(
+            name="ERROR",
+            value="That is not a valid command!"
+        )
+
+    await ctx.send(embed=help_embed)
+
+
+
+########################################CATCH-ERRORS##################################################################
+
+
+@save.error
+async def save_error(ctx, error):
+    if isinstance(error, MissingPermissions):
+        error_embed = discord.Embed(title="ERROR", color = 0x8a3f0a)
+        error_embed.add_field(name='No permission', value="Sorry {}, you do not have permissions to do that!".format(ctx.message.author.name))
+        await ctx.send(embed=error_embed)
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandOnCooldown):
+        error_embed = discord.Embed(title="ERROR", color = 0x8a3f0a)
+        error_embed.add_field(name='Command on Cooldown:', value="Please retry in %s seconds" % int(error.retry_after))
+        await ctx.send(embed=error_embed)
+
+bot.run(TOKEN)
